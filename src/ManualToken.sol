f5119dc953a1b3467ef53966556840c066847f7e
azeezabidoye <azeezabidoye@yahoo.com>
2025-09-12 08:08:21 +0100

    Contract Updated

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.18;
 
 contract ManualToken {
+    mapping(address => uint) private s_balances;
+
     function name() public view returns (string memory) {
         return "ManualToken";
     }
 
     function totalSupply() public pure returns (uint256) {
         return 100 ether;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return s_balances[_owner];
     }
 }
