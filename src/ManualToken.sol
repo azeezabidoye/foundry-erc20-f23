e9295702a78516a7b36132719572190a95aa75c1
azeezabidoye <azeezabidoye@yahoo.com>
2025-09-12 07:59:10 +0100

    Contract Updated

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-contract ManualToken {}
+contract ManualToken {
+    function name() public view returns (string memory) {
+        return "ManualToken";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
+}
