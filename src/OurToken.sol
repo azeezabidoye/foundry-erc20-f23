4586377c9f1aace4470d8945c927decf524abef5
azeezabidoye <azeezabidoye@yahoo.com>
2025-09-15 15:34:23 +0100

    New Contract Created

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol"
 
-contract OurToken {}
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OTK") {
+        _mint(msg.sender, initialSupply);
+    }
+} 
