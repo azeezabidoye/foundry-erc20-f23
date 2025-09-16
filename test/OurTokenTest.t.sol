49978e9e2b82f1f7086a47e2b69bcbef7f9f2095
azeezabidoye <azeezabidoye@yahoo.com>
2025-09-16 14:07:43 +0100

    Test Script Updated

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+import {OurToken} from "../src/OurToken.sol";
 
 contract OurTokenTest is Test {}
