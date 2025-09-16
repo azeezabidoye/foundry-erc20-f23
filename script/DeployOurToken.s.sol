e85ebeddb4d03d604bbe20dc2d625daa592304c9
azeezabidoye <azeezabidoye@yahoo.com>
2025-09-16 12:41:20 +0100

    Deploy Script Updated

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -3,3 +3,11 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script {
+    function run() external {
+        vm.startBroadcast();
+        new OurToken;
+        vm.stopBroadcast();
+    }
+}
